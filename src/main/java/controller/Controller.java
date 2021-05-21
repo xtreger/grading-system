48db3a574ab60c4f2b6dd207c9d792fa724bf42d
Mihai Carare <48069042+xtreger@users.noreply.github.com>
2021-05-21 14:44:09 +0100

    Adding logic for creating a rubric (#4)

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,24 @@
 package controller;
 
+import model.Criteria;
+import model.Rubric;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
 
+    private final List<Rubric> rubricList = new ArrayList<>();
+
+    public Rubric addRubric(String rubricTitle) {
+
+        if (rubricTitle == null) {
+            System.out.println("Rubric Title cannot be null!");
+            return null;
+        }
+
+        Rubric rubric = new Rubric(rubricTitle, new ArrayList<>());
+        rubricList.add(rubric);
+        return rubric;
+    }
 }
